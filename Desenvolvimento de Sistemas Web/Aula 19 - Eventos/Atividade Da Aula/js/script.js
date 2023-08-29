3f9799355aad076828b8d9f7d95fa958a94340d3
Ga12briel <francisco.rocha@aluno.ifsp.edu.br>
2023-08-29 13:20:48 -0300

    Décima Nona Aula

--- Desenvolvimento de Sistemas Web/Aula 19 - Eventos/Atividade Da Aula/js/script.js
+++ Desenvolvimento de Sistemas Web/Aula 19 - Eventos/Atividade Da Aula/js/script.js
@@ -1,5 +1,15 @@
-function produtos() {
-  var produto = document.getElementsByClassName('produto');
-  produto.style.cssText =
-    'display: block;';
+function mouseEmCima() {
+  document.getElementById('opcoes').style.display = 'block'
 }
+
+function mouseSaiuDeCima() {
+  document.getElementById('opcoes').style.display = 'none'
+}
+
+function mouseClick() {
+  document.getElementById('opcoes2').style.display = 'block'
+}
+
+setInterval(() => {
+    document.getElementById('opcoes2').style.display = 'none'
+  }, 5000);
