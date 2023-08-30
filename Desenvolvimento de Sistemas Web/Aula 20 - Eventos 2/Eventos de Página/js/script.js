4589c4071d01ab52bae2b80ed6dc1dd20e180fa4
Ga12briel <francisco.rocha@aluno.ifsp.edu.br>
2023-08-30 09:20:57 -0300

    Vigésima Aula

---
+++ Desenvolvimento de Sistemas Web/Aula 20 - Eventos 2/Eventos de Página/js/script.js
@@ -0,0 +1,11 @@
+function carregou() {
+  alert("Evento onload disparado");
+}
+
+function redimensionou() {
+  console.log("Evento onresize dispado");
+}
+
+function rolou() {
+  console.log("Eventou onscroll disparado");
+}
