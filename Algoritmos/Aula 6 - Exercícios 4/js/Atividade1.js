72ea180d6af9cf77d75ba26a327a9571f65afea5
Ga12briel <francisco.rocha@aluno.ifsp.edu.br>
2023-08-31 11:22:29 -0300

    Sexta Aula

---
+++ Algoritmos/Aula 6 - Exercícios 4/js/Atividade1.js
@@ -0,0 +1,70 @@
+// Exemplo 1
+var count = 0;
+while (count <= 10) {
+  document.write(count);
+  document.write(' ');
+  count++;
+}
+document.write('<br> <br>')
+
+// Exemplo 2
+var count = 5;
+while (count <= 100) {
+  document.write(count);
+  document.write(' ');
+  count++;
+}
+document.write('<br> <br>')
+
+// Exemplo 3
+var count = 0;
+while (count <= 100) {
+  document.write(count);
+  document.write(' ');
+  count+=2;
+}
+
+// Exemplo 4
+document.write('<br> <br>')
+var count = 1;
+while (count <=100) {
+  document.write(count);
+  document.write(' ');
+  count*=2;
+}
+
+// Exemplo 5
+document.write('<br> <br>')
+var count = 100;
+while (count >= 0) {
+  document.write(count);
+  document.write(' ');
+  count--;
+}
+
+// Exemplo 6
+document.write('<br> <br>')
+var count = 100;
+while (count >= 0) {
+  document.write(count);
+  document.write(' ');
+  count-= 2;
+}
+
+// Exemplo 7
+document.write('<br> <br>')
+var count = 100;
+while (count != 50) {
+  document.write(count);
+  document.write(' ');
+  count--;
+}
+
+// Exemplo 8
+// document.write('<br> <br>')
+// var count = 100;
+// while (count = 99) {
+//   document.write(count);
+//   document.write(' ');
+//   count--;
+// }
